520a0d5087be953e38ad5f0693476dc6f302f033
NeoJax <NeoJax@gmail.com>
2017-09-07 08:23:28 -0700

    Fixing up Part 2

--- part-2/searchByCity.js
+++ part-2/searchByCity.js
@@ -2,19 +2,26 @@ const fs = require('fs');
 const city = process.argv[2];
 const clients = JSON.parse(fs.readFileSync("clients.json"));
 let search = [];
+let capitalize = city.toLowerCase().split(" ");
+
+for(let i = 0; i < capitalize.length; i++){
+  capitalize[i] = capitalize[i].charAt(0).toUpperCase() + capitalize[i].slice(1);
+}
+
+capitalize = capitalize.join(" ");
 
 console.log("Finding clients in City \"" + city + "\"...");
 
 for(let i = 0; i < clients.length; i++) {
-  if(clients[i].city.includes(city)){
+  if(clients[i].city.includes(capitalize)){
     search.push(clients[i]);
   }
 }
 
-for(let j = 0; j < search.length; j++) {
-  delete search[j].no_employees;
-  delete search[j].phone;
-  delete search[j].created_at;
+for(let i = 0; i < search.length; i++) {
+  delete search[i].no_employees;
+  delete search[i].phone;
+  delete search[i].created_at;
 }
 
 console.log(search);
--- part-2/searchByCompany.js
+++ part-2/searchByCompany.js
@@ -2,13 +2,28 @@ const fs = require('fs');
 const company = process.argv[2];
 const clients = JSON.parse(fs.readFileSync("clients.json"));
 let search = [];
+let capitalize = company.toLowerCase().split(" ");
+
+for(let i = 0; i < capitalize.length; i++){
+  capitalize[i] = capitalize[i].charAt(0).toUpperCase() + capitalize[i].slice(1);
+}
+
+capitalize = capitalize.join(" ");
 
 console.log("Finding companies with name \"" + company + "\"...");
 
 for(let i = 0; i < clients.length; i++){
-  if(clients[i].company.includes(company)){
+  if(clients[i].company.includes(capitalize)){
     search.push(clients[i]);
   }
+}
+
+for(let i = 0; i < search.length; i++) {
+  delete search[i].rep_name;
+  delete search[i].no_employees;
+  delete search[i].city;
+  delete search[i].state;
+  delete search[i].created_at;
 }
 
 console.log(search);
